9846cfee260f91ff88da322785b1e245cc8a1db2
scott.lan <scott.lan@taisys.com>
2014-12-02 14:26:58 +0800

    fm

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appWordingEditorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appWordingEditorApp
+ */
+angular.module('appWordingEditorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
